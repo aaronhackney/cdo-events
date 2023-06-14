a69d18195e6b538a33c15f87aa038f7298ee429a
aaronhackney <ahack210@gmail.com>
2023-06-14 08:54:48 -0500

    removed hardcoded region

--- cdo-events/get_events.py
+++ cdo-events/get_events.py
@@ -18,9 +18,9 @@ def unzip_file(file_name, download_dir):
     logging.warning(f"Archive {download_dir}/{file_name} deleted")
 
 
-def download_files(api_key: str, file_prefix: str, download_dir: str):
+def download_files(api_key: str, file_prefix: str, download_dir: str, cdo_region: str):
     """ Download files with a matching prefix, only if we have not already downloaded them before"""
-    events = CDOEvents(api_key=api_key, cdo_region="us")
+    events = CDOEvents(api_key=api_key, cdo_region=cdo_region)
     event_list = events.get_background_search_list()
     for event_file in event_list['download_status']:
         # Only consider files with the given prefix/name
@@ -37,8 +37,8 @@ def download_files(api_key: str, file_prefix: str, download_dir: str):
                 logging.warning(f"{download_dir}/{event_file['file_name']} file exists - skipping")
 
 
-def main(api_key: str, file_prefix: str, download_dir: str):
-    download_files(api_key, file_prefix, download_dir)
+def main(api_key: str, file_prefix: str, download_dir: str, cdo_region: str):
+    download_files(api_key, file_prefix, download_dir, cdo_region)
 
 
 if __name__ == "__main__":
@@ -48,6 +48,8 @@ if __name__ == "__main__":
                         help="'--prefix abc123' The filename prefix to match for download")
     parser.add_argument('--data_dir', type=str, required=True,
                         help="'--data_dir /tmp/mydata' The download directory for the event files")
+    parser.add_argument('--region', type=str, default='us',
+                        help="'--region [region]' us, eu, or apj region where your CDO tenant resides")
     args = parser.parse_args()
     api_key = os.environ.get('CDO_API_KEY')
-    main(api_key, args.prefix, args.data_dir)
+    main(api_key, args.prefix, args.data_dir, args.region)
